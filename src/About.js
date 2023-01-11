298e63ab4bed59d5e3cf46c25545281b86fa001d
123Pankaj1 <kumarpankaj031093@gmail.com>
2023-01-11 17:02:10 +0800

    UX changes

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
             <div className="About-us">
                 <h1> About Us </h1>
                 <img src="https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1628677506-101156.png" />
-                <p>ATCS Pvt. Ltd. a Naggaro company</p>
+                <p>Demo POC for Micro Front-Ends</p>
             </div>
         </>
     )
